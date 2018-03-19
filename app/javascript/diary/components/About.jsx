b17410db72abfb9c9c410d2f1cd694bc66417867
Para O'Kelly <hksokelly@gmail.com>
2018-03-19 16:30:23 +0800

    Added router skeleton and about page skeleton

---
+++ app/javascript/diary/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="center-plane">
+      <h2 className="label-text">About</h2>
+      <p>
+        Explanation of this mood diary coming soon.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- app/javascript/diary/components/App.jsx
+++ app/javascript/diary/components/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+} from 'react-router-dom';
 import Header from './Header';
+import About from './About';
 
 const App = () => {
   return (
-    <Header />
+    <React.Fragment>
+      <Header />
+      <BrowserRouter>
+        <Route path="/" component={About} />
+      </BrowserRouter>
+    </React.Fragment>
   );
 };
 
